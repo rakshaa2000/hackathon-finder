c4c8c6e3cb1314ab402bc9eef4da3b25ab07c813
Rakshaa Viswanathan <46165429+rakshaa2000@users.noreply.github.com>
2021-06-29 09:56:57 +0530

    first commit

---
+++ scraper.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import date
+import os
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+client = discord.Client()
+
+URL = 'https://mlh.io/seasons/2021/events'
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, 'html.parser')
+new_results = soup.find_all('div',class_='container feature')
+results = new_results[1].find_all('div',class_='row')
+events = results[0].find_all('div', class_='event')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content=='!hf-mlh':
+        for i in events:
+            # list_events.append((name,dates_full)) 
+            name = (i.find('h3',class_='event-name').text)
+            dates_full = i.find('p',class_="event-date").text   
+            link = i.find('a', class_="event-link")['href']
+            msg = name + ": " + dates_full+ ", "+link
+            await message.channel.send(msg)
+
+client.run(TOKEN)
